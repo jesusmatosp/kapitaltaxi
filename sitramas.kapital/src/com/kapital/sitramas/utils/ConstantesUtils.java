ca4c69446cf6d1de3a1eea73ecc6fb9d05b86159
Jesús Matos Portocarrero <jesus.matosp@gmail.com>
2017-05-01 03:01:57 -0500

    Add Change 29/04/2017

--- sitramas.kapital/src/com/kapital/sitramas/utils/ConstantesUtils.java
+++ sitramas.kapital/src/com/kapital/sitramas/utils/ConstantesUtils.java
@@ -3,5 +3,12 @@ package com.kapital.sitramas.utils;
 public class ConstantesUtils {
 
 	final public static String SUBJECT_CREDENCIALES_UPDATE="KAPITAL SITRAMAS - Actualización de Credenciales";
+	final public static String CREATE_RECORD = "Registro Creado";
+	final public static String ERROR_CREATE = "No se pudo Crear el registro";
+	final public static String DELETE_RECORD = "Registro Eliminado";
+	final public static String ERROR_DELETE = "No se pudo Eliminar el Registro";
+	final public static String UPDATE_RECORD = "Registro Actualizado";
+	final public static String ERROR_UPDATE = "No se pudo Actualizar el Registro";
+	final public static String DUPLICATE_SEND = "El Registro ya fue Enviado";
 	
 }
