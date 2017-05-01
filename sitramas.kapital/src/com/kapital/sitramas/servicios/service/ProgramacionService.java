8861895c472fa131506fcbbd848e62f963336890
Jesús Matos Portocarrero <jesus.matosp@gmail.com>
2017-05-01 03:01:04 -0500

    Add Change 29/04/2017

--- sitramas.kapital/src/com/kapital/sitramas/servicios/service/ProgramacionService.java
+++ sitramas.kapital/src/com/kapital/sitramas/servicios/service/ProgramacionService.java
@@ -1,6 +1,7 @@
 package com.kapital.sitramas.servicios.service;
 
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
 import com.kapital.sitramas.servicios.dto.ProgramacionDTO;
 import com.kapital.sitramas.servicios.dto.ProgramacionPasajeroDTO;
@@ -8,10 +9,16 @@ import com.kapital.sitramas.servicios.dto.ProgramacionPasajeroDTO;
 public interface ProgramacionService {
 
 	public List<ProgramacionDTO> findAllProgramacion() throws SQLException, Exception;
+	public List<ProgramacionDTO> findAllProgramacionBorrador() throws SQLException, Exception;
 	public void saveProgramacion(ProgramacionDTO programacionDTO) throws SQLException, Exception;
 	public ProgramacionDTO findById(Long id)throws SQLException, Exception;
 	public void actualizarProgramacion(ProgramacionDTO programacionDTO) throws SQLException, Exception;
+	public Integer enviarProgramacion(Long idProgramacion) throws SQLException, Exception;
+	public List<ProgramacionDTO> findProgramacionByDates(Date fechaInicio, Date fechaFin) throws SQLException, Exception;
 	
 	public List<ProgramacionPasajeroDTO> findAllProgramacionCliente(Long idProgramacion, Integer version) throws SQLException, Exception;
 	public ProgramacionPasajeroDTO findProgramacionClienteById(Long idProgramacionPasajero) throws SQLException, Exception;
+	public void deleteDetalleProgramacion(Long idDetalleProgramacion) throws SQLException, Exception;
+	
+	
 }
