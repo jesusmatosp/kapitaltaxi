0dc85642cccc2d15223c59584a5313518a1e65ea
Jesús Matos Portocarrero <jesus.matosp@gmail.com>
2017-05-01 03:01:46 -0500

    Add Change 29/04/2017

--- sitramas.kapital/src/com/kapital/sitramas/servicios/service/impl/ProgramacionServiceImpl.java
+++ sitramas.kapital/src/com/kapital/sitramas/servicios/service/impl/ProgramacionServiceImpl.java
@@ -2,7 +2,7 @@ package com.kapital.sitramas.servicios.service.impl;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Date; 
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
