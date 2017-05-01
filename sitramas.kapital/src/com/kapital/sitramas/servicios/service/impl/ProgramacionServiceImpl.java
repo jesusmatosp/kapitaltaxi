66e772ba72774de6f90e81603f6151200239ea7a
Jesús Matos Portocarrero <jesus.matosp@gmail.com>
2017-05-01 03:01:16 -0500

    Add Change 29/04/2017

--- sitramas.kapital/src/com/kapital/sitramas/servicios/service/impl/ProgramacionServiceImpl.java
+++ sitramas.kapital/src/com/kapital/sitramas/servicios/service/impl/ProgramacionServiceImpl.java
@@ -2,15 +2,19 @@ package com.kapital.sitramas.servicios.service.impl;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.kapital.sitramas.be.Cliente;
 import com.kapital.sitramas.be.Programacion;
 import com.kapital.sitramas.be.ProgramacionPasajero;
+import com.kapital.sitramas.common.enums.EstadoProgramacionEnum;
 import com.kapital.sitramas.configuracion.dao.ClienteDAO;
 import com.kapital.sitramas.servicios.dao.ProgramacionDAO;
 import com.kapital.sitramas.servicios.dao.ProgramacionPasajeroDAO;
@@ -81,6 +85,8 @@ public class ProgramacionServiceImpl implements ProgramacionService {
 			  programacionDTO.setIdProgramacion(id);
 			  Cliente cliente = clienteDAO.find(programacionDTO.getIdCliente());
 			  programacionDTO.setStrCliente(cliente.getNombreCliente());
+			  if(programacion.getEstado()==EstadoProgramacionEnum.BORRADOR.getValue())
+				  programacionDTO.setFlgBorrador(true);
 		  }
 		}catch(Exception e){
 			throw e;
@@ -128,6 +134,92 @@ public class ProgramacionServiceImpl implements ProgramacionService {
 			BeanUtils.copyProperties(programacionDTO, programacionPasajero);
 		}
 		return programacionDTO;
+	}
+
+	@Override
+	public void deleteDetalleProgramacion(Long idDetalleProgramacion) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		try{
+			programacionPasajeroDAO.delete(idDetalleProgramacion);
+		}catch(Exception e){
+			throw e;
+		}
+	}
+
+	
+	@Override
+	@Transactional
+	public List<ProgramacionDTO> findAllProgramacionBorrador() throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		List<ProgramacionDTO> lstprogramcionDTO = null;
+		try {
+			List<Programacion> programacion = programacionDAO.allProgramacionBorrador();
+			lstprogramcionDTO = new ArrayList<ProgramacionDTO>();
+			for (Programacion p : programacion) {
+				ProgramacionDTO programacionDTO = new ProgramacionDTO();
+				BeanUtils.copyProperties(programacionDTO, p);
+				programacionDTO.setIdProgramacion(p.getId());
+				programacionDTO.setIdCliente(p.getIdCliente());
+				Cliente cliente = clienteDAO.find(p.getIdCliente());
+				if (cliente != null)
+					programacionDTO.setStrCliente(cliente.getNombreCliente());
+				if(p.getEstado()==EstadoProgramacionEnum.BORRADOR.getValue())
+					programacionDTO.setFlgBorrador(true);
+				lstprogramcionDTO.add(programacionDTO);
+			}
+		} catch (Exception e) {
+			Logger.getLogger(getClass().getName()).log(Level.SEVERE, "SITRAMAS ERROR:", e);
+			throw e;
+		}
+		return lstprogramcionDTO;
+	}
+
+	@Override
+	public Integer enviarProgramacion(Long idProgramacion) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		int code = -1;
+		try{
+			Programacion programacion = programacionDAO.find(idProgramacion);
+			List<Programacion> list = programacionDAO.findProgramacionByDate(programacion.getFechaProgramacion(), programacion.getFechaProgramacion());
+			if(list.size() == 0){
+				programacion.setEstado(EstadoProgramacionEnum.ENVIADO.getValue());
+				programacionDAO.update(programacion);
+				code = 1;
+			}else{
+				code = 0;
+			}
+		}catch(Exception e){
+			Logger.getLogger(getClass().getName()).log(Level.SEVERE, "SITRAMAS ERROR:", e);
+			throw e;
+		}
+		return code;
+	}
+
+	@Override
+	public List<ProgramacionDTO> findProgramacionByDates(Date fechaInicio, Date fechaFin)
+			throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		List<ProgramacionDTO> lstprogramcionDTO = null;
+		try{
+			List<Programacion> programacion = programacionDAO.findProgramacionByDate(fechaInicio, fechaFin);
+			lstprogramcionDTO = new ArrayList<ProgramacionDTO>();
+			for (Programacion p : programacion) {
+				ProgramacionDTO programacionDTO = new ProgramacionDTO();
+				BeanUtils.copyProperties(programacionDTO, p);
+				programacionDTO.setIdProgramacion(p.getId());
+				programacionDTO.setIdCliente(p.getIdCliente());
+				Cliente cliente = clienteDAO.find(p.getIdCliente());
+				if (cliente != null)
+					programacionDTO.setStrCliente(cliente.getNombreCliente());
+				if(p.getEstado()==EstadoProgramacionEnum.BORRADOR.getValue())
+					programacionDTO.setFlgBorrador(true);
+				lstprogramcionDTO.add(programacionDTO);
+			}
+		}catch(Exception e){
+			Logger.getLogger(getClass().getName()).log(Level.SEVERE, "SITRAMAS ERROR:", e);
+			throw e;
+		}
+		return lstprogramcionDTO;
 	}
 	
 	
